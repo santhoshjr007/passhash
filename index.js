725e6971d4ebffacfb8ade3ecea9c3f156a6db00
santhoshjr007 <santhosh.r@kalvium.community>
2025-03-18 11:55:13 +0530

    done

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3010;
+app.use(express.json());
+mongoose.connect("mongodb+srv://santhoshr:12345@tda.7zalu.mongodb.net/ass1?retryWrites=true&w=majority&appName=tda").then(() => console.log('MongoDB connected'))
+.catch(err => console.log('Error connecting to MongoDB:', err));
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+      return res.status(400).json({ success: false, message: 'All fields are required.' });
+  }
+  
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ success: true, message: 'User registered successfully!' });
+} catch (error) {
+    console.error('Error during registration:', error);
+    res.status(500).json({ success: false, message: 'Internal server error.' });
+}
+});
+
+
+
 
 app.use(express.static('static'));
 
@@ -13,3 +46,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
